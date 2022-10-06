dfaa39b79e4b96be8e4644a23c20f3ca29552dd5
zeun <real_jieun99@daum.net>
2022-10-07 00:49:13 +0900

    [#1] init server

---
+++ cashcowServer/cashcow/src/main/java/com/bibimbob/cashcow/CashcowApplication.java
@@ -0,0 +1,13 @@
+package com.bibimbob.cashcow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcowApplication.class, args);
+	}
+
+}
---
+++ cashcowServer/cashcow/src/test/java/com/bibimbob/cashcow/CashcowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibimbob.cashcow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
